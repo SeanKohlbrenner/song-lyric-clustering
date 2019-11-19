6977655874f5e3518379874cf1b0db2c43aa459f
smk1025@wildcats.unh.edu <sean@LAPTOP-VJVPEPJI.localdomain>
2019-11-18 20:43:15 -0500

    test config file

--- music-classifier/get_data.py
+++ music-classifier/get_data.py
@@ -1,2 +1,9 @@
+import configparser
 import spotipy
 
+config = configparser.ConfigParser()
+config.read('../docs/config.ini')
+spotipy_client_id = config['Spotipy']['client_id']
+spotipy_client_secret = config['Spotipy']['client_secret']
+
+print(spotipy_client_id)
