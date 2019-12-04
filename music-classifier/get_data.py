fd3d9019151a8414364e820f7a717efd5b966a91
Sean Kohlbrenner <seankohlbrenner@gmail.com>
2019-12-03 23:59:37 -0500

    Delete get_data.py

--- music-classifier/get_data.py
+++
@@ -1,8 +0,0 @@
-import configparser
-import spotipy
-
-config = configparser.ConfigParser()
-config.read('../docs/config.ini')
-spotipy_client_id = config['Spotipy']['client_id']
-spotipy_client_secret = config['Spotipy']['client_secret']
-
